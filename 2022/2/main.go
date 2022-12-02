a751763309ae22b8898fdd1aab0dd58a7dac1a95
alvin.tan <alvintan95@gmail.com>
2022-12-02 21:13:58 +0800

    2-1

--- 2022/2/main.go
+++ 2022/2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 
 	"golang.org/x/exp/constraints"
 )
@@ -20,15 +21,51 @@ func sortSlice[T constraints.Ordered](s []T) {
 	})
 }
 
+func calculateScore(opponent string, me string) int {
+	if (opponent == "A" && me == "X") || (opponent == "B" && me == "Y") || (opponent == "C" && me == "Z") {
+		return 3
+	}
+
+	if (opponent == "A" && me == "Y") || (opponent == "B" && me == "Z") || (opponent == "C" && me == "X") {
+		return 6
+	}
+	return 0
+}
+
+func handScore(me string) int {
+	switch me {
+	case "X":
+		return 1
+	case "Y":
+		return 2
+	case "Z":
+		return 3
+	default:
+		return 0
+
+	}
+}
+
 func part1() {
 	fileName := "input.txt"
 	file, _ := os.Open(fileName)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
+	score := 0
+	counter := 0
 	for scanner.Scan() {
 		s := scanner.Text()
-		fmt.Println(s)
+		arr := strings.Split(s, "")
+		points := calculateScore(arr[0], arr[2])
+		handScore := handScore(arr[2])
+		score += (points + handScore)
+		fmt.Println(points + handScore)
+		counter++
+		if counter == 111111110 {
+			break
+		}
 	}
+	fmt.Println(score)
 }
 
 func part2() {
